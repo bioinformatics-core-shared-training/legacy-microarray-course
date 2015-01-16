946c6a25f2c8aa9f30fef8d3e684a339693c34c8
MarkDunning <mark.dunning@gmail.com>
2015-01-16 14:08:33 +0000

    Remove methylation material (not mature enough)
    Add aCGH to list of packages

--- installPackages.R
+++ installPackages.R
@@ -2,4 +2,4 @@ source("http://www.bioconductor.org/biocLite.R")
 
 biocLite(c("limma", "affy", "beadarray", "BeadArrayUseCases","GEOquery","affyPLM","hgu95av2.db","ggplot2","beadarrayExampleData", 
            "illuminaHumanv3.db","genefilter","RColorBrewer","GOstats","breastCancerVDX","breastCancerTRANSBIG", "pamr", "DNAcopy","crlmm",
-           "illuminaHumanv1.db","minfi", "impute","marray","preprocessCore","RPMM","sva","IlluminaHumanMethylation450kmanifest","wateRmelon","ChAMP"))
+           "illuminaHumanv1.db","minfi", "impute","marray","preprocessCore","RPMM","sva","IlluminaHumanMethylation450kmanifest","wateRmelon","ChAMP","aCGH"))
